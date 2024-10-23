20f8f1cee2722ba79b7b64ae420851f066a75453
Ana <00001239345161sp@al.educacao.sp.gov.br>
2024-10-23 12:16:07 -0300

    FlashCard

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Filosofia',
+    'De quem é a famosa frase “Penso, logo existo"?',
+    'Descartes'
+)
+
+criaCartao(
+    'História',
+    'De onde é a invenção do chuveiro elétrico?',
+    'Brasil'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais o menor e o maior país do mundo?',
+    'Vaticano e Rússia.'
+)
+
+criaCartao(
+    'Química',
+    'Atualmente, quantos elementos químicos a tabela periódica possui?',
+    '118'
+)
